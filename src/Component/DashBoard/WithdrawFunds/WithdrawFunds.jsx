1b2cdf5e5a6480a355f2ac09b14faba6a23b9543
Mahmoud Saaed Romeh <123378237+Engromeh@users.noreply.github.com>
2024-11-11 22:12:59 +0200

    add addfunds and withedraw in dashbord ya habiby

--- src/Component/DashBoard/WithdrawFunds/WithdrawFunds.jsx
+++ src/Component/DashBoard/WithdrawFunds/WithdrawFunds.jsx
@@ -6,18 +6,35 @@ import vodefon from '../../../../photo/vodefone.svg';
 import bank from '../../../../photo/banx.svg';
 import instpay from '../../../../photo/انستباي.svg';
 import amountIcon from '../../../../photo/amount.svg';
-import totalIcon from '../../../../photo/withdraw.svg'
+import totalIcon from '../../../../photo/withdraw.svg';
+
 const WithdrawFunds = () => {
+  const [selectedMethod, setSelectedMethod] = useState('Vodafone Cash');
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
 
+  const getIconForMethod = (method) => {
+    switch (method) {
+      case 'Vodafone Cash':
+        return vodefon;
+      case 'Bank Transfer':
+        return bank;
+      
+      default:
+        return vodefon;
+    }
+  };
+
+  const handleMethodChange = (event) => {
+    setSelectedMethod(event.target.value);
+  };
+
   return (
     <>
       <div className={withdrawstyles.withdrawContainer}>
         <div className={withdrawstyles.headerSection}>
-
           <button className={withdrawstyles.addWithdrawButton} onClick={openModal}>
             Add Withdraw Account
           </button>
@@ -28,46 +45,62 @@ const WithdrawFunds = () => {
           <form className={withdrawstyles.withdrawForm}>
             <img src={headerLogo} alt="Header Logo" className={withdrawstyles.headerLogo} />
 
+            {/* اختيار الطريقة مع تغيير الأيقونة */}
             <label className={withdrawstyles.formLabel}>Method</label>
             <div className={withdrawstyles.inputWithIcon}>
-              <img src={vodefon} alt="Vodafone Icon" className={withdrawstyles.iconInsideInput} />
-            <select className={withdrawstyles.formInput}>
-              <option>Vodafone Cash</option>
-              <option>Bank Transfer</option>
-              <option>PayPal</option>
-            </select>
-</div>
+              <img
+                src={getIconForMethod(selectedMethod)}
+                alt="Method Icon"
+                className={withdrawstyles.iconInsideInput}
+              />
+              <select
+                className={withdrawstyles.formInput}
+                value={selectedMethod}
+                onChange={handleMethodChange}
+              >
+                <option value="Vodafone Cash">Vodafone Cash</option>
+                <option value="Bank Transfer">Bank Transfer</option>
+                <option value="PayPal">PayPal</option>
+              </select>
+            </div>
+
+            {/* حقل المبلغ */}
             <label className={withdrawstyles.formLabel}>Amount</label>
             <div className={withdrawstyles.inputWithUnit}>
-            <div className={withdrawstyles.inputWithIcon}>
-              <img src={amountIcon} alt="Amount Icon" className={withdrawstyles.iconInsideInput} />
-              <input type="text" className={withdrawstyles.formInput} placeholder="" />
-              <span className={withdrawstyles.inputUnit}>EGP</span>
+              <div className={withdrawstyles.inputWithIcon}>
+                <img src={amountIcon} alt="Amount Icon" className={withdrawstyles.iconInsideInput} />
+                <input type="text" className={withdrawstyles.formInput} placeholder="" />
+                <span className={withdrawstyles.inputUnit}>EGP</span>
+              </div>
             </div>
-</div>
+
+            {/* حقل الإجمالي */}
             <label className={withdrawstyles.formLabel}>Total</label>
             <div className={withdrawstyles.inputWithUnit}>
-            <div className={withdrawstyles.inputWithIcon}>
-              <img src={totalIcon} alt="Total Icon" className={withdrawstyles.iconInsideInput} />
-              <input type="text" className={withdrawstyles.formInput} placeholder="" readOnly />
-              <span className={withdrawstyles.inputUnit}>EGP</span>
+              <div className={withdrawstyles.inputWithIcon}>
+                <img src={totalIcon} alt="Total Icon" className={withdrawstyles.iconInsideInput} />
+                <input type="text" className={withdrawstyles.formInput} placeholder="" readOnly />
+                <span className={withdrawstyles.inputUnit}>EGP</span>
+              </div>
             </div>
-</div>
+
+            {/* الأزرار */}
             <div className={withdrawstyles.buttonsContainer}>
               <button className={withdrawstyles.upgradeButton}>Upgrade</button>
               <button className={withdrawstyles.cancelButton} type="button">Cancel</button>
             </div>
           </form>
 
+          {/* الشعارات السفلية */}
           <div className={withdrawstyles.paymentLogos}>
-          <img src={etselit} alt="logo" />
-          <img src={instpay} alt="logo" />
-          <img src={vodefon} alt="logo" />
-          <img src={bank} alt="logo" />
-
+            <img src={etselit} alt="logo" />
+            <img src={instpay} alt="logo" />
+            <img src={vodefon} alt="logo" />
+            <img src={bank} alt="logo" />
           </div>
         </div>
 
+        {/* المودال */}
         {isModalOpen && (
           <div className={withdrawstyles.modalOverlay}>
             <div className={withdrawstyles.modalContent}>
