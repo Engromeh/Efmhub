c9266bce5a9b27b41eeebaa921ee63109383ee37
Mahmoud Saaed Romeh <123378237+Engromeh@users.noreply.github.com>
2024-11-16 20:46:23 +0200

    edit upgreedaccount  in dashbord

--- src/Component/DashBoard/Upgreedaccount/Upgreedaccount.jsx
+++ src/Component/DashBoard/Upgreedaccount/Upgreedaccount.jsx
@@ -1,33 +1,47 @@
 import upgredacountstyles from './Upgreedaccount.module.css';
-
+import upgred from '../../../../photo/upgreed.svg'
+import orangee from "../../../../photo/orange.svg";
+import weeee from "../../../../photo/we pay.svg";
+import insta from "../../../../photo/انستباي.svg";
+import vode from "../../../../photo/vodefone.svg";
+import bankk from "../../../../photo/banx.svg";
+import moneychack from '../../../../photo/money-check 1.svg'
 const UpgradeAccount = () => {
+  
   return (
     <>
       <h2 className={upgredacountstyles.upgradeTitle}>Upgrade Account</h2>
 
       <div className={upgredacountstyles.upgradeContainer}>
-        {/* عرض الرصيد الحالي */}
         <div className={upgredacountstyles.accountBalance}>
           <div className={upgredacountstyles.balanceInfo}>
             <span className={upgredacountstyles.balanceLabel}>Current Balance</span>
             <span className={upgredacountstyles.balanceAmount}>$155.339 EG</span>
           </div>
-          <img src="/images/balance-icon.png" alt="Balance Icon" className={upgredacountstyles.logoUpgrade} />
+          <img src={upgred} alt="Balance Icon" className={upgredacountstyles.logoUpgrade} />
           <div className={upgredacountstyles.balanceDate}>Date: 09/25</div>
         </div>
 
         <form className={upgredacountstyles.upgradeForm}>
-          {/* اختيار وسيلة الدفع */}
           <label className={upgredacountstyles.formLabel}>Select Payment</label>
-          <div className={upgredacountstyles.selectContainer}>
-            <select className={upgredacountstyles.formInput}>
-              <option>Upgrade Using Balance</option>
-              <option>Credit Card</option>
-              <option>PayPal</option>
-            </select>
-          </div>
+<div className={upgredacountstyles.selectContainer}>
+  <div className={upgredacountstyles.selectWrapper}>
+    <img
+      className={upgredacountstyles.selectIcon}
+      src={moneychack} 
+      alt="icon"
+    />
+    <select className={upgredacountstyles.formInput}>
+      <option>Upgrade Using Balance</option>
+      <option>Credit Card</option>
+      <option>PayPal</option>
+    </select>
+  </div>
+</div>
 
-          {/* اختيار الخطة */}
+          
+       
+
           <label className={upgredacountstyles.formLabel}>Plan</label>
           <div className={upgredacountstyles.selectContainer}>
             <select className={upgredacountstyles.formInput}>
@@ -46,9 +60,12 @@ const UpgradeAccount = () => {
 
         {/* الشعارات */}
         <div className={upgredacountstyles.paymentLogos}>
-          <img src="/images/logo1.png" alt="Logo 1" />
-          <img src="/images/logo2.png" alt="Logo 2" />
-          <img src="/images/logo3.png" alt="Logo 3" />
+          <img src={orangee} alt="Logo 1" />
+          <img src={weeee} alt="Logo 2" />
+          <img src={insta} alt="Logo 3" />
+          <img src={vode} alt="Logo 3" />
+          <img src={bankk} alt="Logo 3" />
+
         </div>
       </div>
     </>
